986f284b043db998bedb4bd0e057f5c1b4dd3bf3
Kinoko_2K <65443692+kinoko2k@users.noreply.github.com>
2022-07-26 21:06:33 +0900

    Update Creatorassist.java

--- src/main/java/com/kinoko2k/creatorassist/Creatorassist.java
+++ src/main/java/com/kinoko2k/creatorassist/Creatorassist.java
@@ -1,17 +1,24 @@
 package com.kinoko2k.creatorassist;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Creatorassist  extends JavaPlugin {
+public final class Creatorassist  extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         getLogger().info("§aCreatorAssistが有効になりました");
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
@@ -43,6 +50,34 @@ public final class Creatorassist  extends JavaPlugin {
             Player player = (Player) sender;
             player.setGameMode(GameMode.SURVIVAL);
             player.sendMessage(ChatColor.WHITE + "ゲームモードを" + ChatColor.GOLD + "スペクテイターモード" + ChatColor.WHITE + "に設定しました");
+            return true;
+        }
+        if(command.getName().equalsIgnoreCase("br")) {
+            Player player = (Player) sender;
+            Inventory inv = player.getInventory();
+            inv.setItem(1, new ItemStack(Material.BARRIER,64));
+            player.sendMessage(ChatColor.WHITE + "バリアブロックを" + ChatColor.GOLD + "64個" + ChatColor.WHITE + "付与しました。");
+            return true;
+        }
+        if (command.getName().equalsIgnoreCase("hideme") && args.length == 1) {
+
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("このコマンドはゲーム内から実行してください！");
+                return true;
+            }
+
+            Player s = (Player) sender;
+
+            if (args.length != 1) {
+                return false;
+            }
+
+            Player target = Bukkit.getPlayerExact(args[0]);
+            if (target == null) {
+                sender.sendMessage("Player " + args[0] + " というプレイヤーは見つかりません！");
+                return true;
+            }
+            target.hidePlayer(s);
             return true;
         }
         return false;
